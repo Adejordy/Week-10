5fd85e1e0dadc65c76d257b2432732c10802fd2e
Alyssa <alyssadejordy@gmail.com>
2024-10-10 23:22:01 -0700

    add paragraph

---
+++ script.js
@@ -0,0 +1,7 @@
+//     create card to hold context
+//     table with rows
+//      item being returned
+//      item size
+//      item color
+//      text box with reason for return
+//      submit button
